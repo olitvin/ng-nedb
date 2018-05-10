5c3adf6a2f5b7331a344168f5fdaa5251e872d06
shamahn <shamahn.litvin@ya.ru>
2018-05-10 21:41:57 +0400

    update factory

--- src/lib/nedb.js
+++ src/lib/nedb.js
@@ -1,16 +1,17 @@
+(function () {
+    'use strict';
+    var Datastore = require('nedb');
 
-'use strict';
-var Datastore = require('nedb');
-
-angular.module('olitvin.nedb', [])
-    .factory('$neDB', ["options", function (options) {
-        var db = null;
-
-        if (!options)
-        {
-            db = new Datastore();
-        } else {
-            db = new Datastore(options);
-        }
-        return db;
-    }]);
+    angular.module('olitvin.nedb', [])
+        .factory('$neDB', function () {
+            return function (options) {
+                var db = null;
+                if (!options) {
+                    db = new Datastore();
+                } else {
+                    db = new Datastore(options);
+                }
+                return db;
+            }
+        });
+});
